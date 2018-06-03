a9002bacc4dba3abd4d735175442c141c906b1cd
huangxuan <huangxuan@xiaoduotech.com>
2018-06-03 23:10:12 +0800

    pirce top list

--- controller/wallList.js
+++ controller/wallList.js
@@ -1,11 +1,24 @@
 import { WallType, ChildType, WallInfo } from "../models/wallcover/index";
 class Crawler {
-    constructor() {
+  constructor() {}
+  async getPriceTop(req, res, next) {
+    try {
+      const data = await WallInfo.find()
+        .sort({ price: -1 })
+        .limit(20);
+      console.log("price top list", data);
+      res.send({
+        error_code: 0,
+        data,
+        total: data.length
+      });
+    } catch (e) {
+      console.error(e);
+      res.send({
+        error_code: 1,
+        message: "系统错误"
+      });
     }
-    async getPriceTop(req, res, next) {
-        const data = await WallInfo.find().sort({price: -1}).limit(20);
-        BinData(0,"Li9wdWJsaWMvaW1hZ2VzLzE1MjY1NTE2NTcxNjQuanBn")
-        
-    }
+  }
 }
-export default new Crawler;
+export default new Crawler();
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-import User from '../controller/user'
-import Crawler from '../controller/crawler';
+import User from "../controller/user";
+import WallList from "../controller/wallList";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-router.get('/v1/index/list', function(req, res, next) {
+router.get("/v1/index/list", function(req, res, next) {
   res.send({
     error_code: 0,
     data: {
       banner: [],
       list: []
     },
-    message: 'success'
-  })
-})
-
-router.get('/users', function(req, res, next) {
-  res.render('index', { title: 'Express users' });
+    message: "success"
+  });
 });
 
-
-router.get('/v1/user', User.addUser);
-router.get('/v1/getcntkitty', Crawler.getCNTKitty)
+router.get("/v1/get_price_top", WallList.getPriceTop);
 
 module.exports = router;
